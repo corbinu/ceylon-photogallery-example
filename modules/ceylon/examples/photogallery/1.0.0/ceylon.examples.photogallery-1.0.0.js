104011bca38a5003bbba32db6f1ccc9664d7494c
Corbin Uselton <corbinu@Corbins-MacBook-Air.local>
2013-06-24 00:44:21 -0400

    add code to display main image

--- modules/ceylon/examples/photogallery/1.0.0/ceylon.examples.photogallery-1.0.0.js
+++ modules/ceylon/examples/photogallery/1.0.0/ceylon.examples.photogallery-1.0.0.js
@@ -1,5 +1,5 @@
 (function(define) { define(function(require, exports, module) {
-var $$METAMODEL$$={"ceylon.examples.photogallery":{"$pkg-shared":"1","PhotoCategory":{"super":{"$md":"ceylon.language","$pk":"ceylon.language","$nm":"Basic"},"$ps":[{"$t":{"$md":"ceylon.language","$pk":"ceylon.language","$nm":"String"},"$mt":"prm","$pt":"v","$nm":"name"},{"$t":{"$md":"ceylon.language","$tp":[{"$mt":"tpm","$pk":"ceylon.examples.photogallery","$nm":"Photo"}],"$pk":"ceylon.language","$nm":"Sequential"},"$mt":"prm","$pt":"v","$nm":"photos"}],"$mt":"cls","$m":{"displayPhotos":{"$t":{"$md":"ceylon.language","$pk":"ceylon.language","$nm":"Anything"},"$ps":[[{"$t":{"$md":"ceylon.language","$pk":"ceylon.language","$nm":"String"},"$mt":"prm","$pt":"v","$nm":"dir"}]],"$mt":"mthd","$an":{"shared":[]},"$nm":"displayPhotos"}},"$nm":"PhotoCategory"},"PhotoJSON":{"super":{"$md":"ceylon.language","$pk":"ceylon.language","$nm":"Basic"},"$ps":[{"$t":{"$md":"ceylon.language","$pk":"ceylon.language","$nm":"String"},"$mt":"prm","$pt":"v","$nm":"src"},{"$t":{"$md":"ceylon.language","$pk":"ceylon.language","$nm":"Integer"},"$mt":"prm","$pt":"v","$nm":"height"},{"$t":{"$md":"ceylon.language","$pk":"ceylon.language","$nm":"Integer"},"$mt":"prm","$pt":"v","$nm":"width"},{"$t":{"$md":"ceylon.language","$pk":"ceylon.language","$nm":"String"},"$mt":"prm","$pt":"v","$nm":"alt"},{"$t":{"$md":"ceylon.language","$pk":"ceylon.language","$nm":"Integer"},"$mt":"prm","$pt":"v","$nm":"span"}],"$mt":"cls","$nm":"PhotoJSON"},"PhotoCategoryJSON":{"super":{"$md":"ceylon.language","$pk":"ceylon.language","$nm":"Basic"},"$ps":[{"$t":{"$md":"ceylon.language","$pk":"ceylon.language","$nm":"String"},"$mt":"prm","$pt":"v","$nm":"name"},{"$t":{"$md":"ceylon.language","$tp":[{"$mt":"tpm","$pk":"ceylon.examples.photogallery","$nm":"PhotoJSON"}],"$pk":"ceylon.language","$nm":"Sequential"},"$mt":"prm","$pt":"v","$nm":"photos"}],"$mt":"cls","$nm":"PhotoCategoryJSON"},"init":{"$t":{"$md":"ceylon.language","$pk":"ceylon.language","$nm":"Anything"},"$ps":[[{"$t":{"$md":"ceylon.language","$pk":"ceylon.language","$nm":"String"},"$mt":"prm","$pt":"v","$nm":"galleryDir"}]],"$mt":"mthd","$an":{"shared":[],"doc":["Run the module `ceylon.examples.photogallery`."]},"$nm":"init"},"Photo":{"super":{"$md":"ceylon.language","$pk":"ceylon.language","$nm":"Basic"},"$ps":[{"$t":{"$md":"ceylon.language","$pk":"ceylon.language","$nm":"String"},"$mt":"prm","$pt":"v","$nm":"src"},{"$t":{"$md":"ceylon.language","$pk":"ceylon.language","$nm":"Integer"},"$mt":"prm","$pt":"v","$nm":"height"},{"$t":{"$md":"ceylon.language","$pk":"ceylon.language","$nm":"Integer"},"$mt":"prm","$pt":"v","$nm":"width"},{"$t":{"$md":"ceylon.language","$pk":"ceylon.language","$nm":"String"},"$mt":"prm","$pt":"v","$nm":"alt"},{"$t":{"$md":"ceylon.language","$pk":"ceylon.language","$nm":"Integer"},"$mt":"prm","$pt":"v","$nm":"span"}],"$mt":"cls","$nm":"Photo"},"Gallery":{"super":{"$md":"ceylon.language","$pk":"ceylon.language","$nm":"Basic"},"$ps":[{"$t":{"$md":"ceylon.language","$pk":"ceylon.language","$nm":"String"},"$mt":"prm","$pt":"v","$nm":"dir"}],"$mt":"cls","$m":{"getCategory":{"$t":{"$pk":"ceylon.examples.photogallery","$nm":"PhotoCategory"},"$ps":[[{"$t":{"$pk":"ceylon.examples.photogallery","$nm":"PhotoCategoryJSON"},"$mt":"prm","$pt":"v","$nm":"categoryJSON"}]],"$mt":"mthd","$nm":"getCategory"},"setCategories":{"$t":{"$md":"ceylon.language","$pk":"ceylon.language","$nm":"Anything"},"$ps":[[{"$t":{"$md":"ceylon.language","$tp":[{"$mt":"tpm","$pk":"ceylon.examples.photogallery","$nm":"PhotoCategoryJSON"}],"$pk":"ceylon.language","$nm":"Sequential"},"$mt":"prm","$pt":"v","$nm":"categoriesJSON"}]],"$mt":"mthd","$an":{"shared":[]},"$nm":"setCategories"},"displayCategories":{"$t":{"$md":"ceylon.language","$pk":"ceylon.language","$nm":"Anything"},"$mt":"mthd","$an":{"shared":[]},"$nm":"displayCategories"}},"$at":{"categories":{"$t":{"$md":"ceylon.language","$tp":[{"$mt":"tpm","$pk":"ceylon.examples.photogallery","$nm":"PhotoCategory"}],"$pk":"ceylon.language","$nm":"Sequential"},"var":"1","$mt":"attr","$an":{"shared":[],"variable":[]},"$nm":"categories"}},"$nm":"Gallery"}},"$mod-deps":["ceylon.language\/0.6"],"$mod-name":"ceylon.examples.photogallery","$mod-version":"1.0.0"};
+var $$METAMODEL$$={"ceylon.examples.photogallery":{"$pkg-shared":"1","PhotoCategory":{"super":{"$md":"ceylon.language","$pk":"ceylon.language","$nm":"Basic"},"$ps":[{"$t":{"$pk":"ceylon.examples.photogallery","$nm":"Gallery"},"$mt":"prm","$pt":"v","$nm":"gallery"},{"$t":{"$md":"ceylon.language","$pk":"ceylon.language","$nm":"String"},"$mt":"prm","$pt":"v","$nm":"name"}],"$mt":"cls","$m":{"displayPhotos":{"$t":{"$md":"ceylon.language","$pk":"ceylon.language","$nm":"Anything"},"$ps":[[{"$t":{"$md":"ceylon.language","$pk":"ceylon.language","$nm":"String"},"$mt":"prm","$pt":"v","$nm":"dir"}]],"$mt":"mthd","$an":{"shared":[]},"$nm":"displayPhotos"},"setPhotos":{"$t":{"$md":"ceylon.language","$pk":"ceylon.language","$nm":"Anything"},"$ps":[[{"$t":{"$md":"ceylon.language","$tp":[{"$mt":"tpm","$pk":"ceylon.examples.photogallery","$nm":"PhotoJSON"}],"$pk":"ceylon.language","$nm":"Sequential"},"$mt":"prm","$pt":"v","$nm":"photosJSON"}]],"$mt":"mthd","$an":{"shared":[]},"$nm":"setPhotos"}},"$at":{"photos":{"$t":{"$md":"ceylon.language","$tp":[{"$mt":"tpm","$pk":"ceylon.examples.photogallery","$nm":"Photo"}],"$pk":"ceylon.language","$nm":"Sequential"},"var":"1","$mt":"attr","$an":{"shared":[],"variable":[]},"$nm":"photos"}},"$nm":"PhotoCategory"},"PhotoJSON":{"super":{"$md":"ceylon.language","$pk":"ceylon.language","$nm":"Basic"},"$ps":[{"$t":{"$md":"ceylon.language","$pk":"ceylon.language","$nm":"String"},"$mt":"prm","$pt":"v","$nm":"src"},{"$t":{"$md":"ceylon.language","$pk":"ceylon.language","$nm":"Integer"},"$mt":"prm","$pt":"v","$nm":"height"},{"$t":{"$md":"ceylon.language","$pk":"ceylon.language","$nm":"Integer"},"$mt":"prm","$pt":"v","$nm":"width"},{"$t":{"$md":"ceylon.language","$pk":"ceylon.language","$nm":"String"},"$mt":"prm","$pt":"v","$nm":"alt"},{"$t":{"$md":"ceylon.language","$pk":"ceylon.language","$nm":"Integer"},"$mt":"prm","$pt":"v","$nm":"span"}],"$mt":"cls","$nm":"PhotoJSON"},"PhotoCategoryJSON":{"super":{"$md":"ceylon.language","$pk":"ceylon.language","$nm":"Basic"},"$ps":[{"$t":{"$md":"ceylon.language","$pk":"ceylon.language","$nm":"String"},"$mt":"prm","$pt":"v","$nm":"name"},{"$t":{"$md":"ceylon.language","$tp":[{"$mt":"tpm","$pk":"ceylon.examples.photogallery","$nm":"PhotoJSON"}],"$pk":"ceylon.language","$nm":"Sequential"},"$mt":"prm","$pt":"v","$nm":"photos"}],"$mt":"cls","$nm":"PhotoCategoryJSON"},"init":{"$t":{"$md":"ceylon.language","$pk":"ceylon.language","$nm":"Anything"},"$ps":[[{"$t":{"$md":"ceylon.language","$pk":"ceylon.language","$nm":"String"},"$mt":"prm","$pt":"v","$nm":"galleryDir"}]],"$mt":"mthd","$an":{"shared":[],"doc":["Run the module `ceylon.examples.photogallery`."]},"$nm":"init"},"Photo":{"super":{"$md":"ceylon.language","$pk":"ceylon.language","$nm":"Basic"},"$ps":[{"$t":{"$md":"ceylon.language","$pk":"ceylon.language","$nm":"String"},"$mt":"prm","$pt":"v","$nm":"src"},{"$t":{"$md":"ceylon.language","$pk":"ceylon.language","$nm":"Integer"},"$mt":"prm","$pt":"v","$nm":"height"},{"$t":{"$md":"ceylon.language","$pk":"ceylon.language","$nm":"Integer"},"$mt":"prm","$pt":"v","$nm":"width"},{"$t":{"$md":"ceylon.language","$pk":"ceylon.language","$nm":"String"},"$mt":"prm","$pt":"v","$nm":"alt"},{"$t":{"$md":"ceylon.language","$pk":"ceylon.language","$nm":"Integer"},"$mt":"prm","$pt":"v","$nm":"span"}],"$mt":"cls","$nm":"Photo"},"Gallery":{"super":{"$md":"ceylon.language","$pk":"ceylon.language","$nm":"Basic"},"$ps":[{"$t":{"$md":"ceylon.language","$pk":"ceylon.language","$nm":"String"},"$mt":"prm","$pt":"v","$nm":"dir"}],"$mt":"cls","$m":{"displayPhoto":{"$t":{"$md":"ceylon.language","$pk":"ceylon.language","$nm":"Anything"},"$ps":[[{"$t":{"$pk":"ceylon.examples.photogallery","$nm":"Photo"},"$mt":"prm","$pt":"v","$nm":"photo"}]],"$mt":"mthd","$an":{"shared":[]},"$nm":"displayPhoto"},"getCategory":{"$t":{"$pk":"ceylon.examples.photogallery","$nm":"PhotoCategory"},"$ps":[[{"$t":{"$pk":"ceylon.examples.photogallery","$nm":"PhotoCategoryJSON"},"$mt":"prm","$pt":"v","$nm":"categoryJSON"}]],"$mt":"mthd","$nm":"getCategory"},"setCategories":{"$t":{"$md":"ceylon.language","$pk":"ceylon.language","$nm":"Anything"},"$ps":[[{"$t":{"$md":"ceylon.language","$tp":[{"$mt":"tpm","$pk":"ceylon.examples.photogallery","$nm":"PhotoCategoryJSON"}],"$pk":"ceylon.language","$nm":"Sequential"},"$mt":"prm","$pt":"v","$nm":"categoriesJSON"}]],"$mt":"mthd","$an":{"shared":[]},"$nm":"setCategories"},"init":{"$t":{"$md":"ceylon.language","$pk":"ceylon.language","$nm":"Anything"},"$mt":"mthd","$nm":"init"},"displayCategory":{"$t":{"$md":"ceylon.language","$pk":"ceylon.language","$nm":"Anything"},"$ps":[[{"$t":{"$md":"ceylon.language","$pk":"ceylon.language","$nm":"String"},"$mt":"prm","$pt":"v","$nm":"name"}]],"$mt":"mthd","$an":{"shared":[]},"$nm":"displayCategory"}},"$at":{"categories":{"$t":{"$md":"ceylon.language","$tp":[{"$mt":"tpm","$pk":"ceylon.examples.photogallery","$nm":"PhotoCategory"}],"$pk":"ceylon.language","$nm":"Sequential"},"var":"1","$mt":"attr","$an":{"shared":[],"variable":[]},"$nm":"categories"}},"$nm":"Gallery"}},"$mod-deps":["ceylon.language\/0.6"],"$mod-name":"ceylon.examples.photogallery","$mod-version":"1.0.0"};
 var $$$cl1=require('ceylon/language/0.6/ceylon.language-0.6');
 function init(galleryDir$2){
     /*Begin dynamic block*/
@@ -35,36 +35,43 @@ function Gallery(dir, $$gallery){
                 return $$$cl1.getFinished();
             }
         },{Absent:{t:$$$cl1.Null},Element:{t:PhotoCategory}}).sequence;
-        $$gallery.displayCategories();
+        init$13();
     }
     $$gallery.setCategories=setCategories;
     setCategories.$$metamodel$$={mod:$$METAMODEL$$,$t:{t:$$$cl1.Anything},$ps:[{$nm:'categoriesJSON',$mt:'prm',$t:{t:$$$cl1.Sequential,a:{Element:{t:PhotoCategoryJSON}}}}],$an:function(){return[$$$cl1.shared()];},pkg:'ceylon.examples.photogallery',d:$$METAMODEL$$['ceylon.examples.photogallery']['Gallery']['$m']['setCategories']};//setCategories.$$targs$$={Arguments:{t:$$$cl1.Tuple,a:{Rest:{t:$$$cl1.Empty},First:{t:$$$cl1.Sequential,a:{Element:{t:PhotoCategoryJSON}}},Element:{t:$$$cl1.Sequential,a:{Element:{t:PhotoCategoryJSON}}}}},Return:{t:$$$cl1.Anything}};
-    function displayCategories(){
-        var it$13 = $$gallery.categories.iterator();
-        var category$14;while ((category$14=it$13.next())!==$$$cl1.getFinished()){
-            category$14.displayPhotos($$gallery.dir);
-        }
-    }
-    $$gallery.displayCategories=displayCategories;
-    displayCategories.$$metamodel$$={mod:$$METAMODEL$$,$t:{t:$$$cl1.Anything},$ps:[],$an:function(){return[$$$cl1.shared()];},pkg:'ceylon.examples.photogallery',d:$$METAMODEL$$['ceylon.examples.photogallery']['Gallery']['$m']['displayCategories']};//displayCategories.$$targs$$={Arguments:{t:$$$cl1.Empty},Return:{t:$$$cl1.Anything}};
-    function getCategory$12(categoryJSON$15){
-        var photos$16=$$$cl1.Comprehension(function(){
-            var it$17=categoryJSON$15.photos.iterator();
-            var photo$18=$$$cl1.getFinished();
-            var next$photo$18=function(){return photo$18=it$17.next();}
-            next$photo$18();
-            return function(){
-                if(photo$18!==$$$cl1.getFinished()){
-                    var photo$18$19=photo$18;
-                    var tmpvar$20=Photo(photo$18$19.src,photo$18$19.height,photo$18$19.width,photo$18$19.alt,photo$18$19.span);
-                    next$photo$18();
-                    return tmpvar$20;
-                }
-                return $$$cl1.getFinished();
+    function displayCategory(name$14){
+        var it$15 = $$gallery.categories.iterator();
+        var category$16;while ((category$16=it$15.next())!==$$$cl1.getFinished()){
+            if(name$14.equals(category$16.name)){
+                category$16.displayPhotos($$gallery.dir);
+                return ;
             }
-        },{Absent:{t:$$$cl1.Null},Element:{t:Photo}}).sequence;
-        return PhotoCategory(categoryJSON$15.name,photos$16);
+        }
+        /*Begin dynamic block*/
+        (typeof alert==='undefined'||alert===null?$$$cl1.throwexc('Undefined or null reference: alert'):alert)($$$cl1.String("Could not find category with name: ",35).plus(name$14).valueOf());
+        /*End dynamic block*/
+    }
+    $$gallery.displayCategory=displayCategory;
+    displayCategory.$$metamodel$$={mod:$$METAMODEL$$,$t:{t:$$$cl1.Anything},$ps:[{$nm:'name',$mt:'prm',$t:{t:$$$cl1.String}}],$an:function(){return[$$$cl1.shared()];},pkg:'ceylon.examples.photogallery',d:$$METAMODEL$$['ceylon.examples.photogallery']['Gallery']['$m']['displayCategory']};//displayCategory.$$targs$$={Arguments:{t:$$$cl1.Tuple,a:{Rest:{t:$$$cl1.Empty},First:{t:$$$cl1.String},Element:{t:$$$cl1.String}}},Return:{t:$$$cl1.Anything}};
+    function displayPhoto(photo$17){
+        /*Begin dynamic block*/
+        (typeof jQuery==='undefined'||jQuery===null?$$$cl1.throwexc('Undefined or null reference: jQuery'):jQuery)($$$cl1.String(".display-photo",14).valueOf()).attr($$$cl1.String("src",3).valueOf(),$$gallery.dir.plus($$$cl1.String("/",1)).plus(photo$17.src).valueOf()).attr($$$cl1.String("alt",3).valueOf(),photo$17.alt.valueOf());
+        /*End dynamic block*/
+    }
+    $$gallery.displayPhoto=displayPhoto;
+    displayPhoto.$$metamodel$$={mod:$$METAMODEL$$,$t:{t:$$$cl1.Anything},$ps:[{$nm:'photo',$mt:'prm',$t:{t:Photo}}],$an:function(){return[$$$cl1.shared()];},pkg:'ceylon.examples.photogallery',d:$$METAMODEL$$['ceylon.examples.photogallery']['Gallery']['$m']['displayPhoto']};//displayPhoto.$$targs$$={Arguments:{t:$$$cl1.Tuple,a:{Rest:{t:$$$cl1.Empty},First:{t:Photo},Element:{t:Photo}}},Return:{t:$$$cl1.Anything}};
+    function getCategory$12(categoryJSON$18){
+        var category$19=PhotoCategory($$gallery,categoryJSON$18.name);
+        category$19.setPhotos(categoryJSON$18.photos);
+        return category$19;
     };getCategory$12.$$metamodel$$={mod:$$METAMODEL$$,$t:{t:PhotoCategory},$ps:[{$nm:'categoryJSON',$mt:'prm',$t:{t:PhotoCategoryJSON}}],pkg:'ceylon.examples.photogallery',d:$$METAMODEL$$['ceylon.examples.photogallery']['Gallery']['$m']['getCategory']};//getCategory$12.$$targs$$={Arguments:{t:$$$cl1.Tuple,a:{Rest:{t:$$$cl1.Empty},First:{t:PhotoCategoryJSON},Element:{t:PhotoCategoryJSON}}},Return:{t:PhotoCategory}};
+    function init$13(){
+        var firstCategory$20=$$gallery.categories.get((0));
+        var firstCategory$21;
+        if((firstCategory$21=firstCategory$20)!==null){
+            $$gallery.displayCategory(firstCategory$21.name);
+        }
+    };init$13.$$metamodel$$={mod:$$METAMODEL$$,$t:{t:$$$cl1.Anything},$ps:[],pkg:'ceylon.examples.photogallery',d:$$METAMODEL$$['ceylon.examples.photogallery']['Gallery']['$m']['init']};//init$13.$$targs$$={Arguments:{t:$$$cl1.Empty},Return:{t:$$$cl1.Anything}};
     return $$gallery;
 }
 Gallery.$$metamodel$$={mod:$$METAMODEL$$,'super':{t:$$$cl1.Basic},satisfies:[],pkg:'ceylon.examples.photogallery',d:$$METAMODEL$$['ceylon.examples.photogallery']['Gallery']};
@@ -76,18 +83,51 @@ function $init$Gallery(){
 }
 exports.$init$Gallery=$init$Gallery;
 $init$Gallery();
-function PhotoCategory(name, photos, $$photoCategory){
+function PhotoCategory(gallery, name, $$photoCategory){
     $init$PhotoCategory();
     if ($$photoCategory===undefined)$$photoCategory=new PhotoCategory.$$;
-    $$photoCategory.photos=photos;
-    function displayPhotos(dir$21){
-        $$$cl1.print($$$cl1.String("adding photos",13));
-        var it$22 = $$photoCategory.photos.iterator();
-        var photo$23;while ((photo$23=it$22.next())!==$$$cl1.getFinished()){
+    $$photoCategory.gallery=gallery;
+    $$photoCategory.name=name;
+    var photos=$$$cl1.getEmpty();
+    $$$cl1.defineAttr($$photoCategory,'photos',function(){return photos;},function(photos$22){return photos=photos$22;});
+    function setPhotos(photosJSON$23){
+        $$photoCategory.photos=$$$cl1.Comprehension(function(){
+            var it$24=photosJSON$23.iterator();
+            var photo$25=$$$cl1.getFinished();
+            var next$photo$25=function(){return photo$25=it$24.next();}
+            next$photo$25();
+            return function(){
+                if(photo$25!==$$$cl1.getFinished()){
+                    var photo$25$26=photo$25;
+                    var tmpvar$27=Photo(photo$25$26.src,photo$25$26.height,photo$25$26.width,photo$25$26.alt,photo$25$26.span);
+                    next$photo$25();
+                    return tmpvar$27;
+                }
+                return $$$cl1.getFinished();
+            }
+        },{Absent:{t:$$$cl1.Null},Element:{t:Photo}}).sequence;
+    }
+    $$photoCategory.setPhotos=setPhotos;
+    setPhotos.$$metamodel$$={mod:$$METAMODEL$$,$t:{t:$$$cl1.Anything},$ps:[{$nm:'photosJSON',$mt:'prm',$t:{t:$$$cl1.Sequential,a:{Element:{t:PhotoJSON}}}}],$an:function(){return[$$$cl1.shared()];},pkg:'ceylon.examples.photogallery',d:$$METAMODEL$$['ceylon.examples.photogallery']['PhotoCategory']['$m']['setPhotos']};//setPhotos.$$targs$$={Arguments:{t:$$$cl1.Tuple,a:{Rest:{t:$$$cl1.Empty},First:{t:$$$cl1.Sequential,a:{Element:{t:PhotoJSON}}},Element:{t:$$$cl1.Sequential,a:{Element:{t:PhotoJSON}}}}},Return:{t:$$$cl1.Anything}};
+    function displayPhotos(dir$28){
+        var firstPhoto$29=$$photoCategory.photos.get((0));
+        var firstPhoto$30;
+        if((firstPhoto$30=firstPhoto$29)!==null){
+            $$photoCategory.gallery.displayPhoto(firstPhoto$30);
+        }
+        var i$31=(0);
+        var setI$31=function(i$32){return i$31=i$32;};
+        var it$33 = $$photoCategory.photos.iterator();
+        var photo$34;while ((photo$34=it$33.next())!==$$$cl1.getFinished()){
             /*Begin dynamic block*/
-            $$$cl1.print($$$cl1.String("adding photo: ",14).plus(photo$23.src));
-            (typeof jQuery==='undefined'||jQuery===null?$$$cl1.throwexc('Undefined or null reference: jQuery'):jQuery)($$$cl1.String(".image-grid",11).valueOf()).append($$$cl1.String("<li class=\"span",15).plus(photo$23.span.string).plus($$$cl1.String("\"><a href=\"#\" class=\"thumbnail\"><img src=\"",42)).plus(dir$21).plus($$$cl1.String("/",1)).plus(photo$23.src).plus($$$cl1.String("\" alt=\"",7)).plus(photo$23.alt).plus($$$cl1.String("\" width=\"",9)).plus(photo$23.width.string).plus($$$cl1.String("\" height=\"",10)).plus(photo$23.height.string).plus($$$cl1.String("\"></a></li>",11)).valueOf());
+            $$$cl1.print($$$cl1.String("adding photo: ",14).plus(photo$34.src));
+            (typeof jQuery==='undefined'||jQuery===null?$$$cl1.throwexc('Undefined or null reference: jQuery'):jQuery)($$$cl1.String(".image-grid",11).valueOf()).append($$$cl1.String("<li class=\"span",15).plus(photo$34.span.string).plus($$$cl1.String("\"><a href=\"#\" class=\"thumbnail photo",36)).plus(i$31.string).plus($$$cl1.String("\"><img src=\"",12)).plus(dir$28).plus($$$cl1.String("/",1)).plus(photo$34.src).plus($$$cl1.String("\" alt=\"",7)).plus(photo$34.alt).plus($$$cl1.String("\" width=\"",9)).plus(photo$34.width.string).plus($$$cl1.String("\" height=\"",10)).plus(photo$34.height.string).plus($$$cl1.String("\"></a></li>",11)).valueOf());
+            (typeof jQuery==='undefined'||jQuery===null?$$$cl1.throwexc('Undefined or null reference: jQuery'):jQuery)($$$cl1.String(".photo",6).plus(i$31.string).valueOf()).click($$$cl1.$JsCallable(function (){
+                return $$photoCategory.gallery.displayPhoto(photo$34);
+            },[],{Arguments:{t:$$$cl1.Empty},Return:{t:$$$cl1.Anything}}));
             /*End dynamic block*/
+            (oldi$35=i$31,i$31=oldi$35.successor,oldi$35);
+            var oldi$35;
         }
     }
     $$photoCategory.displayPhotos=displayPhotos;
--- source/ceylon/examples/photogallery/run.ceylon
+++ source/ceylon/examples/photogallery/run.ceylon
@@ -16,38 +16,73 @@ class Gallery(shared String dir) {
 	
 	shared void setCategories(PhotoCategoryJSON[] categoriesJSON) {
 		categories = [for (category in categoriesJSON) getCategory(category)];
-		displayCategories();
+		init();
 	}
 	
-	shared void displayCategories() {
+	shared void displayCategory(String name) {
 		for (category in categories) {
-			category.displayPhotos(dir);
+			if (name == category.name) {
+				category.displayPhotos(dir);
+				return;
+			}
+		}
+		dynamic {
+			alert("Could not find category with name: " + name);
+		}
+	}
+	
+	shared void displayPhoto(Photo photo) {
+		dynamic {
+			// Should be shown here on click
+			jQuery(".display-photo").attr("src", dir + "/" + photo.src).attr("alt", photo.alt);
 		}
 	}
 	
 	PhotoCategory getCategory(PhotoCategoryJSON categoryJSON) {
-		Photo[] photos  = [ for ( photo in categoryJSON.photos) Photo(photo.src, photo.height, photo.width, photo.alt, photo.span) ];
-			
-		return PhotoCategory(categoryJSON.name, photos);
+		PhotoCategory category = PhotoCategory(this, categoryJSON.name);
+		category.setPhotos(categoryJSON.photos);
+		return category;
+	}
+	
+	void init() {
+		PhotoCategory? firstCategory = categories[0];
+		if (exists firstCategory) {
+			displayCategory(firstCategory.name);
+		}
 	}
 	
 }
 
-class PhotoCategory(shared String name, shared Photo[] photos) {
+class PhotoCategory(shared Gallery gallery, shared String name) {
+	shared variable Photo[] photos = {};
+	
+	shared void setPhotos(PhotoJSON[] photosJSON) {
+		photos  = [ for ( photo in photosJSON) Photo(photo.src, photo.height, photo.width, photo.alt, photo.span) ];
+	}
 	
 	shared void displayPhotos(String dir) {
-		print("adding photos");
+		Photo? firstPhoto = photos[0];
+		if (exists firstPhoto) {
+			gallery.displayPhoto(firstPhoto);
+		}
+		
+		variable Integer i = 0;
 		for (photo in photos) {
 			dynamic {
 				print("adding photo: " + photo.src);
 				jQuery(".image-grid").append("<li class=\"span" + photo.span.string 
-					+ "\"><a href=\"#\" class=\"thumbnail\"><img src=\"" 
+					+ "\"><a href=\"#\" class=\"thumbnail photo" + i.string + "\"><img src=\"" 
 					+ dir + "/" + photo.src + "\" alt=\"" + photo.alt 
 					+ "\" width=\"" + photo.width.string 
 					+ "\" height=\"" + photo.height.string + "\"></a></li>");
+				// Should be binding to a photo click here and passing the photo
+				jQuery(".photo"+i.string).click( () => gallery.displayPhoto(photo));
 			}
+			i++;
 		}
 	}
+	
+	
 }
 class Photo(shared String src, shared Integer height, shared Integer width, shared String alt, shared Integer span) {}
 
